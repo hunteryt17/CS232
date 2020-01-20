00eb742d9203bc8e46eb0eac62d198a429c1b284
Hunter <hunterythompson@gmail.com>
2020-01-20 15:00:55 -0600

    test3

--- assign2/assignment2.py
+++ assign2/assignment2.py
@@ -103,7 +103,7 @@ def problem1(admin_str):
     h = md5(state=md5dig, count=num_bits_orig)
     md5dig_admin= h.update(admin_str)
     n_hash = h.hexdigest()
-    url_new = url_split1[0] + b'=' + bytes.fromhex(n_hash)# + str0 + padding0 + admin_str
+    url_new = url_split1[0] + b'=' + bytes.fromhex(n_hash) + str0 + padding0 + admin_str
     print(padding0)
     print(url_new)
     return flag
