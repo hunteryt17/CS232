1a7c61c4264e2b614fe4bbf8989ad88e70ab3bdd
Hunter <hunterythompson@gmail.com>
2020-01-20 23:34:45 -0600

    test3

--- assign2/assignment2.py
+++ assign2/assignment2.py
@@ -102,6 +102,7 @@ def problem1(admin_str):
     h = md5(state=md5dig, count=num_bits_orig)
     md5dig_admin= h.update(admin_str)
     n_hash = h.hexdigest()
+
     for s in range(1, 65):
         padding0 = padding((len(str0)+s)*8)
         url_new = url_split1[0] + b'=' + bytes.fromhex(n_hash) + str0 + padding0 + admin_str
@@ -110,6 +111,8 @@ def problem1(admin_str):
         try:
             print(make_query('one', 'hunterythompson', url_new))
             return make_query('one', 'hunterythompson', url_new)
+    
+    return
 
 
 ################################################################################
