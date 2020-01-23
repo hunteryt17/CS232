ab76842051d4dfb7b2d414c8cd4f0a0a6a94aecc
Hunter <hunterythompson@gmail.com>
2020-01-23 00:29:38 -0600

    Prob 5

--- assign2/assignment2.py
+++ assign2/assignment2.py
@@ -230,7 +230,8 @@ def problem5():
     X_byte_large = X_byte + f_byte1 + f_byte1
     X_int = int(X_byte, 16)
     X_large_int = int(X_byte_large, 16)
-    X_int_fcrt = Decimal(X_int**(1./3.)) #take cube root of smallest value in range
+    X_int_fcrt = Decimal(X_int) #take cube root of smallest value in range
+    X_int_fcrt = X_int_fcrt ** Decimal(1/3)
     X_cbrt = X_int_fcrt.to_integral_value() #+ Decimal(1) #add 1 to int val of cube root
     X_cbrt_new = X_cbrt + 1
     print(X_cbrt)
